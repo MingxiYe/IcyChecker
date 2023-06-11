8cea84a3ee9fc5a17e567f077e62791ca82a4cc0
MingxiYe <yemx6@mail2.sysu.edu.cn>
2023-06-11 15:07:12 +0800

    feat: update msgbuilder with targeted function for fuzzing

--- cmd/substate-cli/fuzz/MsgBuilder.go
+++ cmd/substate-cli/fuzz/MsgBuilder.go
@@ -75,3 +75,80 @@ func MsgBuilder(targetedContracts []string, timestamp uint64, localUsers []strin
 
 	return addressResults, msgResults, msgStrings, nil
 }
+
+/*
+ * generate function calls for each targeted contracts
+ * given stroage index that are expected to be interfered
+ */
+func MsgBuilder2(targetedContract2Function map[string][]string, timestamp uint64, localUsers []string, localContracts []string) ([]string, []string, []string, error) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println(err)
+			printCallStackIfError()
+		}
+	}()
+
+	var (
+		addressResults []string
+		msgResults     []string
+		msgStrings     []string
+		data           []byte
+		abi            *ABI
+		err            error
+	)
+
+	for contract, signatureList := range targetedContract2Function {
+		path := GlobalABIPath + contract + ".json"
+		if data, err = readFile(path); err != nil {
+			continue
+		}
+		if abi, err = newAbi(data); err != nil {
+			continue
+		}
+
+		for _, fun := range ([]*Function)(*abi) {
+			if fun.Type != "function" ||
+				fun.Constant == true ||
+				!containByList(signatureList, fun.Sig()) ||
+				fun.Statemutability == "pure" ||
+				fun.Statemutability == "view" {
+				continue
+			}
+
+			for j := 0; j < RAND_CASE_SCALE; j++ {
+				if len(fun.Inputs) <= 0 {
+					if hex_str, suberr := abi_gen.Parse_GenMsg(fun.Sig()); suberr == nil {
+						addressResults = append(addressResults, contract)
+						msgResults = append(msgResults, hex_str)
+						msgStrings = append(msgStrings, fun.Sig())
+					}
+					break
+				}
+				if ret, err := fun.Inputs.fuzz(timestamp, localUsers, localContracts); err == nil {
+					temp := fun.Sig() + ":[" + ret.(string) + "]"
+					if hex_str, suberr := abi_gen.Parse_GenMsg(temp); suberr == nil {
+						addressResults = append(addressResults, contract)
+						msgResults = append(msgResults, hex_str)
+						msgStrings = append(msgStrings, temp)
+					}
+				} else {
+					addressResults = append(addressResults, contract)
+					msgResults = append(msgResults, "0xcaffee")
+					msgStrings = append(msgStrings, "0xcaffee")
+				}
+			}
+			break
+		}
+	}
+
+	return addressResults, msgResults, msgStrings, nil
+}
+
+func containByList(list []string, item string) bool {
+	for _, listItem := range list {
+		if item == listItem {
+			return true
+		}
+	}
+	return false
+}
